63463d6d1ce3f7d910f0a3fc98b222c11df1ea38
luis-i-reyes-castro <luis.i.reyes.castro@gmail.com>
2026-04-18 17:37:42 -0500

    Merge class `StateMachine` into `CaseHandler`

--- casehandler.py
+++ casehandler.py
@@ -10,15 +10,17 @@ Case Handler
 """
 
 from inspect import currentframe
+from transitions import State
 
 from sofia_utils.io import load_json_file
+from sofia_utils.printing import ( print_ind,
+                                   print_sep )
 from wa_agents.agent import Agent
 from wa_agents.basemodels import *
 from wa_agents.case_handler_base import CaseHandlerBase
 from wa_agents.whatsapp_functions import markdown_to_whatsapp
 
 from domain_knowledge.dk_basemodels import RCImageAnalysis
-from state_machine import StateMachine
 from tool_server import ToolServer
 
 
@@ -34,6 +36,68 @@ class CaseHandler(CaseHandlerBase) :
                            "qwen/qwen2.5-vl-32b-instruct:free",
                            "mistralai/pixtral-12b" ]
     
+    @classmethod
+    def get_state_machine_config(cls) -> tuple[ list[State], list[dict] ] :
+        
+        states = [
+        
+        # Information-gathering states
+        State( "idle",                on_enter = [ "ask_for_model_having_nothing" ]),
+        State( "have_model_no_image", on_enter = [ "set_model",
+                                                   "ask_for_image"                ]),
+        State( "have_image_no_model", on_enter = [ "ask_for_model_having_image"   ]),
+        
+        # Single-task agents
+        State( "image_agent", on_enter = [ "set_model",
+                                           "call_image_agent"          ],
+                              on_exit  = [ "clear_image_agent_context" ]),
+        State( "match_agent", on_enter = [ "call_match_agent"          ],
+                              on_exit  = [ "clear_match_agent_context" ]),
+        
+        # Main agent
+        State( "main_agent", on_enter = [ "call_main_agent" ])
+        
+        ]
+        
+        transitions = [
+        
+        # From state: idle
+        { "source"  : "idle",
+          "trigger" : "has_model_choice",
+          "dest"    : "have_model_no_image" },
+        { "source"  : "idle",
+          "trigger" : "has_image",
+          "dest"    : "have_image_no_model" },
+        
+        # From state: have_model_no_image
+        { "source"  : "have_model_no_image",
+          "trigger" : "has_image",
+          "dest"    : "image_agent" },
+        
+        # From state: have_image_no_model
+        { "source"  : "have_image_no_model",
+          "trigger" : "has_model_choice",
+          "dest"    : "image_agent" },
+        
+        # From state: image_agent
+        { "source"  : "image_agent",
+          "trigger" : "has_image_analysis",
+          "dest"    : "match_agent" },
+        
+        # From state: match_agent
+        { "source"  : "match_agent",
+          "trigger" : "has_match_tool_call",
+          "dest"    : "main_agent" },
+        
+        # From state: main_agent
+        { "source"  : "main_agent",
+          "trigger" : "has_image",
+          "dest"    : "image_agent" },
+        
+        ]
+        
+        return states, transitions
+    
     def __init__( self,
                   operator : WhatsAppMetaData,
                   user     : WhatsAppContact,
@@ -41,17 +105,106 @@ class CaseHandler(CaseHandlerBase) :
         
         super().__init__( operator, user, debug)
         
-        # Initialize state machine and tool server
-        self.state_machine = StateMachine(debug)
-        self.tool_server   = ToolServer(debug)
+        # Drone model choice
+        self.model_choice : str = None
         
-        # Initialize images cache
-        self.imgs_cache : dict[ str, bytes] = {}
-        
-        # Declare agents
+        # Agents
         self.image_agent : Agent = None
         self.match_agent : Agent = None
         self.main_agent  : Agent = None
+        
+        # Agent contexts
+        self.image_agent_context : list[Message] = []
+        self.match_agent_context : list[Message] = []
+        self.main_agent_context  : list[Message] = []
+        
+        # Images cache (for image agent)
+        self.imgs_cache : dict[ str, bytes] = {}
+        
+        # State machine
+        states, transitions = self.get_state_machine_config()
+        self.init_machine( states, transitions, initial = "idle")
+        
+        # Functions to clear contexts (for on_exit)
+        self.clear_image_agent_context = lambda : self.image_agent_context.clear()
+        self.clear_match_agent_context = lambda : self.match_agent_context.clear()
+        
+        # Tool server
+        self.tool_server = ToolServer(debug)
+        
+        return
+    
+    def reset_state_machine(self) -> None :
+        
+        self.state = "idle"
+        self.model_choice = None
+        self.image_agent_context.clear()
+        self.match_agent_context.clear()
+        self.main_agent_context.clear()
+        
+        return
+    
+    def ingest_message( self, message : Message) -> None :
+        """
+        Ingest a single message and fire corresponding triggers \\
+        Args:
+            message : Instance of a subclass of Message
+        """
+        
+        # ---------------------------------------------------------------------------------
+        # BEFORE TRANSITION
+        
+        if self.debug :
+            print_sep()
+            print("State Machine Message Ingestion")
+            print_ind( f"[>] State k-1: {self.state}", 1)
+        
+        msg_has_image = False
+        
+        # ---------------------------------------------------------------------------------
+        # TRANSITION HAPPENS HERE
+        
+        if isinstance( message, UserMsg) :
+            
+            if isinstance( message, UserInteractiveReplyMsg) :
+                self.model_choice = message.choice.id
+                self.trigger("has_model_choice")
+            
+            msg_has_image = isinstance( message, UserContentMsg) and message.media \
+                            and message.media.mime.startswith("image")
+            if msg_has_image :
+                self.trigger("has_image")
+        
+        elif isinstance( message, AssistantMsg) :
+            
+            if message.agent == "image" :
+                self.trigger("has_image_analysis")
+            
+            elif ( message.agent == "match" ) and message.tool_calls :
+                self.trigger("has_match_tool_call")
+        
+        # ---------------------------------------------------------------------------------
+        # AFTER TRANSITION
+        
+        if self.debug :
+            print_ind( f"[>] State k  : {self.state}", 1)
+        
+        # If message is meant for the user eyes only then return
+        if isinstance( message, ServerMsg) and message.user_eyes :
+            return
+        
+        # Else append message to corresponding agent's context
+        elif self.state in ( 'idle',
+                             'have_model_no_image',
+                             'have_image_no_model',
+                             'image_agent') and msg_has_image :
+            self.image_agent_context.append(message)
+        
+        elif self.state == 'match_agent' :
+            self.match_agent_context.append(message)
+        
+        elif self.state == 'main_agent' :
+            self.main_agent_context.append(message)
         
         return
     
@@ -165,13 +318,13 @@ class CaseHandler(CaseHandlerBase) :
         if not self.case_context :
             self.context_build()
         
-        # Retrieve actions from state machine
-        actions = self.state_machine.get_actions()
+        # Retrieve actions from current state
+        actions = set( self.machine.get_state(self.state).on_enter )
         
         # If necessary then set model
         if "set_model" in actions :
             if not self.tool_server.dkdb.model :
-                model = self.state_machine.model_choice
+                model = self.model_choice
                 self.case_set_drone_model(model)
                 self.tool_server.dkdb.set_model(model)
         
@@ -282,7 +435,7 @@ class CaseHandler(CaseHandlerBase) :
         # PHASE 1: GENERATE IMAGE ANALYSIS
         
         # Prepare image analysis agent context
-        image_agent_context = self.state_machine.image_agent_context
+        image_agent_context = self.image_agent_context
         # Prepare images cache
         for msg_with_image in image_agent_context :
             image_filename = msg_with_image.media.name
@@ -372,7 +525,7 @@ class CaseHandler(CaseHandlerBase) :
         # STAGE 1: GENERATE INITIAL MATCH AGENT RESPONSE
         
         # Prepare match agent context
-        match_agent_context = self.state_machine.match_agent_context
+        match_agent_context = self.match_agent_context
         
         # Generate response
         message = self.match_agent.get_response( context    = match_agent_context,
@@ -465,7 +618,7 @@ class CaseHandler(CaseHandlerBase) :
             self.setup_main_agent()
         
         # Prepare main agent context
-        main_agent_context = self.state_machine.main_agent_context
+        main_agent_context = self.main_agent_context
         
         # Generate main agent response
         message = self.main_agent.get_response( context    = main_agent_context,
